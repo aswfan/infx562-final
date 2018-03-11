1e72c7caf3df5ee236adac5a96c1ca9c3351e882
wwei29 <weijiawei009@hotmail.com>
2018-03-10 21:02:07 -0800

    fix page transitions - Wei Wei

--- js/pagetransitions.js
+++ js/pagetransitions.js
@@ -1,11 +1,11 @@
-var PageTransitions = (function() {
+var PageTransitions = (function () {
 
-	var $main = $( '#pt-main' ),
-		$pages = $main.children( 'div.pt-page' ),
-		$button1 = $( '#button1' ),
-		$button2 = $( '#button2' ),
-		$button3 = $( '#button3' ),
-		$button4 = $( '#button4' ),
+	var $main = $('#pt-main'),
+		$pages = $main.children('div.pt-page'),
+		$button1 = $('#button1'),
+		$button2 = $('#button2'),
+		$button3 = $('#button3'),
+		$button4 = $('#button4'),
 		animcursor = 1,
 		pagesCount = $pages.length,
 		current = 0,
@@ -13,41 +13,43 @@ var PageTransitions = (function() {
 		endCurrPage = false,
 		endNextPage = false,
 		animEndEventNames = {
-			'WebkitAnimation' : 'webkitAnimationEnd',
-			'OAnimation' : 'oAnimationEnd',
-			'msAnimation' : 'MSAnimationEnd',
-			'animation' : 'animationend'
+			'WebkitAnimation': 'webkitAnimationEnd',
+			'OAnimation': 'oAnimationEnd',
+			'msAnimation': 'MSAnimationEnd',
+			'animation': 'animationend'
 		},
 		// animation end event name
-		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+		animEndEventName = animEndEventNames[Modernizr.prefixed('animation')],
 		// support css animations
 		support = Modernizr.cssanimations;
-	
+
 	function init() {
 
-		$pages.each( function() {
-			var $page = $( this );
-			$page.data( 'originalClassList', $page.attr( 'class' ) );
-		} );
+		console.log($pages);
 
-		$pages.eq( current ).addClass( 'pt-page-current' );
+		$pages.each(function () {
+			var $page = $(this);
+			$page.data('originalClassList', $page.attr('class'));
+		});
 
-		$button1.on( 'click', function() {
+		$pages.eq(current).addClass('pt-page-current');
+
+		$button1.on('click', function () {
 			nextPage(1);
 		});
 
-		$button2.on( 'click', function() {
+		$button2.on('click', function () {
 			nextPage(2);
 		});
-		
-		$button3.on( 'click', function() {
+
+		$button3.on('click', function () {
 			nextPage(3);
 		});
-		
-		$button4.on( 'click', function() {
+
+		$button4.on('click', function () {
 			nextPage(4);
 		});
-		
+
 		//$iterate.on( 'click', function() {
 		//	if( isAnimating ) {
 		//		return false;
@@ -61,17 +63,39 @@ var PageTransitions = (function() {
 
 	}
 
-	function nextPage(options ) {
+	function nextPage(options) {
 		var animation = (options.animation) ? options.animation : options;
 
-		if( isAnimating ) {
+		if (isAnimating) {
 			return false;
 		}
 
 		isAnimating = true;
-		
-		var $currPage = $pages.eq( current );
 
+		var $currPage = $pages.eq(current);
+
+
+		if (options == 1) {
+			current = 0;
+			var $nextPage = $pages.eq(0).addClass('pt-page-current'),
+				outClass = '', inClass = '';
+		}
+		else if (options == 2) {
+			current = 1;
+			var $nextPage = $pages.eq(1).addClass('pt-page-current'),
+				outClass = '', inClass = '';
+		}
+		else if (options == 3) {
+			current = 2;
+			var $nextPage = $pages.eq(2).addClass('pt-page-current'),
+				outClass = '', inClass = '';
+		}
+		else if (options == 4) {
+			current = 3;
+			var $nextPage = $pages.eq(3).addClass('pt-page-current'),
+				outClass = '', inClass = '';
+		}
+		/*
 		if(options.showPage){
 			if( options.showPage < pagesCount - 1 ) {
 				current = options.showPage;
@@ -88,11 +112,17 @@ var PageTransitions = (function() {
 				current = 0;
 			}
 		}
-
+		console.log("current" + current);
+		console.log("next page" + options);
+		
 		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
 			outClass = '', inClass = '';
+		*/
 
-		switch( animation ) {
+		console.log($nextPage);
+
+
+		switch (animation) {
 
 			case 1:
 				outClass = 'pt-page-moveToLeft';
@@ -112,45 +142,45 @@ var PageTransitions = (function() {
 				break;
 		}
 
-		$currPage.addClass( outClass ).on( animEndEventName, function() {
-			$currPage.off( animEndEventName );
+		$currPage.addClass(outClass).on(animEndEventName, function () {
+			$currPage.off(animEndEventName);
 			endCurrPage = true;
-			if( endNextPage ) {
-				onEndAnimation( $currPage, $nextPage );
+			if (endNextPage) {
+				onEndAnimation($currPage, $nextPage);
 			}
-		} );
+		});
 
-		$nextPage.addClass( inClass ).on( animEndEventName, function() {
-			$nextPage.off( animEndEventName );
+		$nextPage.addClass(inClass).on(animEndEventName, function () {
+			$nextPage.off(animEndEventName);
 			endNextPage = true;
-			if( endCurrPage ) {
-				onEndAnimation( $currPage, $nextPage );
+			if (endCurrPage) {
+				onEndAnimation($currPage, $nextPage);
 			}
-		} );
+		});
 
-		if( !support ) {
-			onEndAnimation( $currPage, $nextPage );
+		if (!support) {
+			onEndAnimation($currPage, $nextPage);
 		}
 
 	}
 
-	function onEndAnimation( $outpage, $inpage ) {
+	function onEndAnimation($outpage, $inpage) {
 		endCurrPage = false;
 		endNextPage = false;
-		resetPage( $outpage, $inpage );
+		resetPage($outpage, $inpage);
 		isAnimating = false;
 	}
 
-	function resetPage( $outpage, $inpage ) {
-		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
-		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+	function resetPage($outpage, $inpage) {
+		$outpage.attr('class', $outpage.data('originalClassList'));
+		$inpage.attr('class', $inpage.data('originalClassList') + ' pt-page-current');
 	}
 
 	init();
 
-	return { 
-		init : init,
-		nextPage : nextPage,
+	return {
+		init: init,
+		nextPage: nextPage,
 	};
 
 })();
